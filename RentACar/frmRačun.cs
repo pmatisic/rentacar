51eb4ac7eca035e1510988d250e5dc39a7f51e72
Petar <pmatisic@foi.hr>
2023-01-03 02:03:10 +0100

    Dodane forme za prikaz podataka -ažurirana imena -dodane konfiguracije

--- RentACar/frmRačun.cs
+++ RentACar/frmRačun.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace RentACar
 {
-    public partial class Form1 : Form
+    public partial class frmRačun : Form
     {
-        public Form1()
+        public frmRačun()
         {
             InitializeComponent();
         }
--- RentACar/frmUpiti.Designer.cs
+++ RentACar/frmUpiti.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RentACar
 {
-    partial class Form1
+    partial class frmUpiti
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmUpiti
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "frmUpiti";
+            this.Text = "Upiti";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
     }
 }
-
