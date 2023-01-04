5ee6bfbdeaf52a6d936b00c1a5cb1532bb20a85c
Petar <pmatisic@foi.hr>
2023-01-04 06:10:14 +0100

    Dodane datoteke

---
+++ RentACar/RentACar/frmVozilo.designer.cs
@@ -0,0 +1,46 @@
+﻿namespace RentACar
+{
+    partial class frmVozilo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmVozilo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "frmVozilo";
+            this.Text = "Vozila";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
