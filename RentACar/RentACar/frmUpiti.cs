3fcfd8367fe0b9f4c05dfea45919091cc3bd3ec1
Petar <pmatisic@foi.hr>
2023-01-06 03:31:36 +0100

    Male promjene -popravljeni upiti

--- RentACar/RentACar/frmUpiti.cs
+++ RentACar/RentACar/frmUpiti.cs
@@ -1,20 +1,104 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
 
 namespace RentACar
 {
     public partial class frmUpiti : Form
     {
+        NpgsqlConnection con = new NpgsqlConnection("Server=localhost;Port=5432;Database=postgres;Username=postgres;Password=1234;");
+
         public frmUpiti()
         {
             InitializeComponent();
+        }
+
+        public void prikazi_podatke1()
+        {
+            con.Open();
+            NpgsqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select model.naziv as \"Naziv modela\", najam.datumpocetkanajma as \"Početak najma\", najam.datumzavrsetkanajma as \"Završetak najma\" from model, vozilo, najam where vozilo.idmodel=model.\"IDmodel\" and najam.idvozilo=vozilo.\"IDvozilo\" and model.\"naziv\"='" + tbModel.Text + "'";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(cmd);
+            da.Fill(dt);
+            dgvUpit1.DataSource = dt;
+            con.Close();
+        }
+        public void prikazi_podatke2()
+        {
+            con.Open();
+            NpgsqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select klijent.ime as \"Ime klijenta\", klijent.prezime as \"Prezime klijenta\", proizvodac.naziv as \"Proizvođač auta\", model.naziv as \"Naziv modela auta\", najam.datumpocetkanajma as \"Početak najma\", najam.datumzavrsetkanajma as \"Završetak najma\" from klijent join najam on (klijent.\"IDklijent\" = najam.idklijent) join vozilo on (vozilo.\"IDvozilo\"=najam.idvozilo) join model on (model.\"IDmodel\"=vozilo.idmodel) join proizvodac on (proizvodac.\"IDproizvodac\"=model.idproizvodac) where klijent.ime='" + tbKlijent.Text + "'";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(cmd);
+            da.Fill(dt);
+            dgvUpit2.DataSource = dt;
+            con.Close();
+        }
+        public void prikazi_podatke3()
+        {
+            con.Open();
+            NpgsqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select proizvodac.naziv as \"Proizvođač auta\", model.naziv as \"Model auta\", klijent.ime as \"Ime klijenta\", klijent.prezime as \"Prezime kijenta\", najam.datumpocetkanajma as \"Početak najma\" from klijent, najam, vozilo, model, proizvodac where najam.idklijent=klijent.\"IDklijent\" and najam.idvozilo=vozilo.\"IDvozilo\" and vozilo.idmodel=model.\"IDmodel\" and model.idproizvodac=proizvodac.\"IDproizvodac\" and najam.datumzavrsetkanajma is null;";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(cmd);
+            da.Fill(dt);
+            dgvUpit3.DataSource = dt;
+            con.Close();
+        }
+        public void prikazi_podatke4()
+        {
+            con.Open();
+            NpgsqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select (select grad from lokacija where lokacija.\"IDlokacija\"=vozilo.idlokacijavozila) as \"Grad\", vozilo.registracijskeoznake as \"Registracijska oznaka\", proizvodac.naziv as \"Proizvođač auta\", (select model.naziv from model where model.\"IDmodel\"=vozilo.idmodel) as \"Model auta\" from vozilo join najam on (najam.idvozilo=vozilo.\"IDvozilo\") join model on (model.\"IDmodel\"=vozilo.idmodel) join proizvodac on (proizvodac.\"IDproizvodac\"=model.idproizvodac) where najam.datumzavrsetkanajma is not null and (select lokacija.\"grad\" from lokacija where lokacija.\"IDlokacija\"=vozilo.idlokacijavozila)='" + tbLokacija.Text + "' except select (select grad from lokacija where lokacija.\"IDlokacija\"=vozilo.idlokacijavozila) as \"Grad\", vozilo.registracijskeoznake as \"Registracijska oznaka\", proizvodac.naziv as \"Proizvođač auta\", (select model.naziv from model where Model.\"IDmodel\"=vozilo.idmodel) as \"Model auta\" from vozilo join najam on (najam.idvozilo=vozilo.\"IDvozilo\") join model on (model.\"IDmodel\"=vozilo.idmodel) join proizvodac on (proizvodac.\"IDproizvodac\"=model.idproizvodac) where najam.datumzavrsetkanajma is null and (select lokacija.\"grad\" from lokacija where lokacija.\"IDlokacija\"=vozilo.idlokacijavozila)='" + tbLokacija.Text + "'";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(cmd);
+            da.Fill(dt);
+            dgvUpit4.DataSource = dt;
+            con.Close();
+        }
+
+        private void frmUpiti_Load(object sender, EventArgs e)
+        {
+            //prikazi_podatke();
+        }
+
+        private void btnPrikazi1_Click(object sender, EventArgs e)
+        {
+            prikazi_podatke1();
+        }
+
+        private void btnPrikazi2_Click(object sender, EventArgs e)
+        {
+            prikazi_podatke2();
+        }
+
+        private void btnPrikazi3_Click(object sender, EventArgs e)
+        {
+            prikazi_podatke3();
+        }
+
+        private void btnPrikazi4_Click(object sender, EventArgs e)
+        {
+            prikazi_podatke4();
         }
     }
 }
--- RentACar/RentACar/frmUpiti.designer.cs
+++ RentACar/RentACar/frmUpiti.designer.cs
@@ -28,19 +28,281 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgvUpit4 = new System.Windows.Forms.DataGridView();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnPrikazi4 = new System.Windows.Forms.Button();
+            this.tbLokacija = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dgvUpit3 = new System.Windows.Forms.DataGridView();
+            this.btnPrikazi3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dgvUpit2 = new System.Windows.Forms.DataGridView();
+            this.btnPrikazi2 = new System.Windows.Forms.Button();
+            this.tbKlijent = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgvUpit1 = new System.Windows.Forms.DataGridView();
+            this.btnPrikazi1 = new System.Windows.Forms.Button();
+            this.tbModel = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUpit4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUpit3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUpit2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUpit1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgvUpit4
+            // 
+            this.dgvUpit4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.dgvUpit4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUpit4.Location = new System.Drawing.Point(57, 792);
+            this.dgvUpit4.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvUpit4.Name = "dgvUpit4";
+            this.dgvUpit4.RowHeadersWidth = 51;
+            this.dgvUpit4.Size = new System.Drawing.Size(905, 142);
+            this.dgvUpit4.TabIndex = 35;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(53, 773);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(287, 16);
+            this.label7.TabIndex = 34;
+            this.label7.Text = "Pregled vozila po lokacijama koji se ne koriste:";
+            // 
+            // btnPrikazi4
+            // 
+            this.btnPrikazi4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnPrikazi4.Location = new System.Drawing.Point(283, 732);
+            this.btnPrikazi4.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPrikazi4.Name = "btnPrikazi4";
+            this.btnPrikazi4.Size = new System.Drawing.Size(100, 28);
+            this.btnPrikazi4.TabIndex = 33;
+            this.btnPrikazi4.Text = "Prikaži";
+            this.btnPrikazi4.UseVisualStyleBackColor = true;
+            this.btnPrikazi4.Click += new System.EventHandler(this.btnPrikazi4_Click);
+            // 
+            // tbLokacija
+            // 
+            this.tbLokacija.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.tbLokacija.Location = new System.Drawing.Point(118, 734);
+            this.tbLokacija.Margin = new System.Windows.Forms.Padding(4);
+            this.tbLokacija.Name = "tbLokacija";
+            this.tbLokacija.Size = new System.Drawing.Size(132, 22);
+            this.tbLokacija.TabIndex = 32;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(53, 738);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 16);
+            this.label6.TabIndex = 31;
+            this.label6.Text = "Lokacija:";
+            // 
+            // dgvUpit3
+            // 
+            this.dgvUpit3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.dgvUpit3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUpit3.Location = new System.Drawing.Point(55, 558);
+            this.dgvUpit3.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvUpit3.Name = "dgvUpit3";
+            this.dgvUpit3.RowHeadersWidth = 51;
+            this.dgvUpit3.Size = new System.Drawing.Size(905, 142);
+            this.dgvUpit3.TabIndex = 30;
+            // 
+            // btnPrikazi3
+            // 
+            this.btnPrikazi3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnPrikazi3.Location = new System.Drawing.Point(283, 523);
+            this.btnPrikazi3.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPrikazi3.Name = "btnPrikazi3";
+            this.btnPrikazi3.Size = new System.Drawing.Size(100, 28);
+            this.btnPrikazi3.TabIndex = 29;
+            this.btnPrikazi3.Text = "Prikaži";
+            this.btnPrikazi3.UseVisualStyleBackColor = true;
+            this.btnPrikazi3.Click += new System.EventHandler(this.btnPrikazi3_Click);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(53, 529);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(187, 16);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Vozila koja se trenutno koriste:";
+            // 
+            // dgvUpit2
+            // 
+            this.dgvUpit2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.dgvUpit2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUpit2.Location = new System.Drawing.Point(55, 337);
+            this.dgvUpit2.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvUpit2.Name = "dgvUpit2";
+            this.dgvUpit2.RowHeadersWidth = 51;
+            this.dgvUpit2.Size = new System.Drawing.Size(905, 142);
+            this.dgvUpit2.TabIndex = 27;
+            // 
+            // btnPrikazi2
+            // 
+            this.btnPrikazi2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnPrikazi2.Location = new System.Drawing.Point(283, 278);
+            this.btnPrikazi2.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPrikazi2.Name = "btnPrikazi2";
+            this.btnPrikazi2.Size = new System.Drawing.Size(100, 28);
+            this.btnPrikazi2.TabIndex = 26;
+            this.btnPrikazi2.Text = "Prikaži";
+            this.btnPrikazi2.UseVisualStyleBackColor = true;
+            this.btnPrikazi2.Click += new System.EventHandler(this.btnPrikazi2_Click);
+            // 
+            // tbKlijent
+            // 
+            this.tbKlijent.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.tbKlijent.Location = new System.Drawing.Point(118, 280);
+            this.tbKlijent.Margin = new System.Windows.Forms.Padding(4);
+            this.tbKlijent.Name = "tbKlijent";
+            this.tbKlijent.Size = new System.Drawing.Size(132, 22);
+            this.tbKlijent.TabIndex = 25;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 317);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(255, 16);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Sva unajmljena vozila određenog klijenta:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 284);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 16);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Klijent:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 83);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(220, 16);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Naziv modela vozila i datumi najma:";
+            // 
+            // dgvUpit1
+            // 
+            this.dgvUpit1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.dgvUpit1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUpit1.Location = new System.Drawing.Point(55, 103);
+            this.dgvUpit1.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvUpit1.Name = "dgvUpit1";
+            this.dgvUpit1.RowHeadersWidth = 51;
+            this.dgvUpit1.Size = new System.Drawing.Size(905, 137);
+            this.dgvUpit1.TabIndex = 21;
+            // 
+            // btnPrikazi1
+            // 
+            this.btnPrikazi1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnPrikazi1.Location = new System.Drawing.Point(283, 39);
+            this.btnPrikazi1.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPrikazi1.Name = "btnPrikazi1";
+            this.btnPrikazi1.Size = new System.Drawing.Size(100, 28);
+            this.btnPrikazi1.TabIndex = 20;
+            this.btnPrikazi1.Text = "Prikaži";
+            this.btnPrikazi1.UseVisualStyleBackColor = true;
+            this.btnPrikazi1.Click += new System.EventHandler(this.btnPrikazi1_Click);
+            // 
+            // tbModel
+            // 
+            this.tbModel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.tbModel.Location = new System.Drawing.Point(118, 41);
+            this.tbModel.Margin = new System.Windows.Forms.Padding(4);
+            this.tbModel.Name = "tbModel";
+            this.tbModel.Size = new System.Drawing.Size(132, 22);
+            this.tbModel.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 45);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Model:";
             // 
             // frmUpiti
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1013, 1055);
+            this.Controls.Add(this.dgvUpit4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnPrikazi4);
+            this.Controls.Add(this.tbLokacija);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dgvUpit3);
+            this.Controls.Add(this.btnPrikazi3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dgvUpit2);
+            this.Controls.Add(this.btnPrikazi2);
+            this.Controls.Add(this.tbKlijent);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgvUpit1);
+            this.Controls.Add(this.btnPrikazi1);
+            this.Controls.Add(this.tbModel);
+            this.Controls.Add(this.label1);
             this.Name = "frmUpiti";
             this.Text = "Upiti";
+            this.Load += new System.EventHandler(this.frmUpiti_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUpit4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUpit3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUpit2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUpit1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvUpit4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnPrikazi4;
+        private System.Windows.Forms.TextBox tbLokacija;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dgvUpit3;
+        private System.Windows.Forms.Button btnPrikazi3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dgvUpit2;
+        private System.Windows.Forms.Button btnPrikazi2;
+        private System.Windows.Forms.TextBox tbKlijent;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgvUpit1;
+        private System.Windows.Forms.Button btnPrikazi1;
+        private System.Windows.Forms.TextBox tbModel;
+        private System.Windows.Forms.Label label1;
     }
 }
